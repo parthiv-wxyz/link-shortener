9580f94c1dc905cbcc4183843faae1ee672247f4
Parthiv <pparthivprakash1@gmail.com>
2025-10-27 20:55:26 +0530

    BASE_URL on frontend

--- frontend/src/api/links.ts
+++ frontend/src/api/links.ts
@@ -1,6 +1,6 @@
 import type { Link } from "../types/link";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const createShortLink = async (originalUrl: string): Promise<Link> => {
   const res = await fetch(`${BASE_URL}/api/links`, {
