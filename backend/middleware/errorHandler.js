8b26b89e85fb75882378333cd98e7e4a6dc978aa
Parthiv <pparthivprakash1@gmail.com>
2025-11-03 14:24:43 +0530

    test4

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,9 +1,9 @@
-export const errorHandler = (err, res) => {
-  console.error(err.stack);
+// export const errorHandler = (err, res) => {
+//   console.error(err.stack);
 
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode).json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
-  });
-};
+//   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+//   res.status(statusCode).json({
+//     message: err.message,
+//     stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+//   });
+// };
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,14 @@ import dotenv from "dotenv";
 import cors from "cors";
 import sequelize from "./config/db.js";
 import linkRoutes from "./routes/linkRoutes.js";
-import { errorHandler } from "./middleware/errorHandler.js";
+// import { errorHandler } from "./middleware/errorHandler.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(errorHandler);
+// app.use(errorHandler);
 app.use(express.json());
 app.use("/", linkRoutes);
 
