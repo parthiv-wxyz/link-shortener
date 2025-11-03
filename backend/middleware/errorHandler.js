322e010989785bf10937e8e06a66b694c9c22dec
Parthiv <pparthivprakash1@gmail.com>
2025-11-03 14:45:52 +0530

    test5

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,9 +1,10 @@
-// export const errorHandler = (err, res) => {
-//   console.error(err.stack);
+export const errorHandler = (err, req, res, next) => {
+  console.error(err?.stack || err);
+  const statusCode = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
 
-//   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-//   res.status(statusCode).json({
-//     message: err.message,
-//     stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
-//   });
-// };
+  res.status(statusCode).json({
+    success: false,
+    message: err?.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err?.stack,
+  });
+};
